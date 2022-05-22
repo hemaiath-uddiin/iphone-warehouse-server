b061809ea95444e343f313c26da9815a79861f92
hemaith-uddin <hemaithisifat@gmail.com>
2022-05-22 22:45:13 +0600

    addded

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express() ;
   app.use(cors());
   app.use(express.json()) ; 
   
-const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.iyeky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyeky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() { 
   try{ 
